96ad0ab835a9a5e5661b7692fd38add5e44f15e3
Lollipop <291950304@qq.com>
2021-08-03 23:36:12 +0800

    使用插件自动导入mixins和variables.less样式文件

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [
+        path.join(__dirname, './src/assets/styles/variables.less'),
+        path.join(__dirname, './src/assets/styles/mixins.less')
+      ]
+    }
+  }
+}
