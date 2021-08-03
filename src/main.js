b2fcf1d827c2cc79b092e03e0dbd872ea3017015
Lollipop <291950304@qq.com>
2021-08-03 23:45:27 +0800

    使用normalize重置样式，添加公共样式

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入 normalize 重置样式
+import 'normalize.css'
+// 引入公共样式
+import '@/assets/styles/common.less'
+
 createApp(App).use(store).use(router).mount('#app')
