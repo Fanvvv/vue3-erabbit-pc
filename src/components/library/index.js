185e52efe01d01b602309b5a43029e1b7b4679bd
Lollipop <291950304@qq.com>
2021-08-07 14:09:05 +0800

    面板封装，提取首页的公用面板进行复用

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,10 +1,12 @@
 // 全局注册组件
 import XtxSkeleton from './xtx-skeleton'
 import XtxCarousel from './xtx-carousel'
+import XtxMore from './xtx-more'
 
 export default {
   install (app) {
     app.component(XtxSkeleton.name, XtxSkeleton)
     app.component(XtxCarousel.name, XtxCarousel)
+    app.component(XtxMore.name, XtxMore)
   }
 }
