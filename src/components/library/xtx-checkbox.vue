406d0a0a9355db649f9215c45e2462de057ca359
Lollipop <291950304@qq.com>
2021-08-10 14:32:09 +0800

    封装复选框组件

---
+++ src/components/library/xtx-checkbox.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="xtx-checkbox" @click="changChecked()">
+  <i v-if="checked" class="iconfont icon-checked"></i>
+  <i v-else class="iconfont icon-unchecked"></i>
+  <span v-if="$slots.default"><slot></slot></span>
+</div>
+</template>
+
+<script>
+import { useVModel } from '@vueuse/core'
+
+export default {
+  name: 'XtxCheckbox',
+  props: {
+    modelValue: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup (props, { emit }) {
+    // 使用useVModel实现双向数据绑定v-model指令
+    // 1. 使用props接收modelValue
+    // 2. 使用useVModel来包装props中的modelValue属性数据
+    // 3. 在使用checked.value就是使用父组件数据
+    // 4. 在使用checked.value = '数据' 赋值，触发emit('update:modelvalue', '数据')
+    const checked = useVModel(props, 'modelValue', emit)
+    const changChecked = () => {
+      const newValue = !checked.value
+      checked.value = newValue
+      emit('chang', newValue)
+    }
+    return { checked, changChecked }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.xtx-checkbox {
+  display: inline-block;
+  margin-right: 2px;
+  .icon-checked {
+    color: @xtxColor;
+    ~ span {
+      color: @xtxColor;
+    }
+  }
+  i {
+    position: relative;
+    top: 1px;
+  }
+  span {
+    margin-left: 2px;
+  }
+}
+</style>
