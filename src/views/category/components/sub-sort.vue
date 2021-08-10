3e687099177fc8eb4a9e6bdca252256635b200c1
Lollipop <291950304@qq.com>
2021-08-10 15:04:36 +0800

    封装排序组件

---
+++ src/views/category/components/sub-sort.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class='sub-sort'>
+    <div class="sort">
+      <a
+        href="javascript:;"
+        :class = "{ active: sortParams.sortField === null }"
+        @click = "changeSort(null)">默认排序</a>
+      <a
+        href="javascript:;"
+        :class = "{ active: sortParams.sortField === 'publishTime' }"
+        @click = "changeSort('publishTime')">最新商品</a>
+      <a
+        href="javascript:;"
+        :class = "{ active: sortParams.sortField === 'orderNum' }"
+        @click = "changeSort('orderNum')">最高人气</a>
+      <a
+        href="javascript:;"
+        :class = "{ active: sortParams.sortField === 'evaluateNum' }"
+        @click = "changeSort('evaluateNum')">评论最多</a>
+      <a href="javascript:;" @click="changeSort('price')">
+        价格排序
+        <i class="arrow up" :class="{ active: sortParams.sortField === 'price' && sortParams.sortMethod === 'asc' }"/>
+        <i class="arrow down" :class="{ active: sortParams.sortField === 'price' && sortParams.sortMethod === 'desc' }"/>
+      </a>
+    </div>
+    <div class="check">
+      <XtxCheckbox>仅显示有货商品</XtxCheckbox>
+      <XtxCheckbox>仅显示特惠商品</XtxCheckbox>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+export default {
+  name: 'SubSort',
+  setup () {
+    const sortParams = reactive({
+      inventory: false,
+      onlyDiscount: false,
+      sortField: null,
+      sortMethod: null
+    })
+    const changeSort = (sortField) => {
+      // 排序逻辑
+      if (sortField === 'price') {
+        sortParams.sortField = sortField
+        // 第一次点击
+        if (sortParams.sortMethod === null) {
+          sortParams.sortMethod = 'desc'
+        } else {
+          // 其他情况根据当前排序取反
+          sortParams.sortMethod = sortParams.sortMethod === 'desc' ? 'asc' : 'desc'
+        }
+      } else {
+        // 如果排序未改变停止逻辑
+        if (sortParams.sortField === sortField) return
+        sortParams.sortField = sortField
+        sortParams.sortMethod = null
+      }
+    }
+    return { sortParams, changeSort }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.sub-sort {
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .sort {
+    display: flex;
+    a {
+      height: 30px;
+      line-height: 28px;
+      border: 1px solid #e4e4e4;
+      padding: 0 20px;
+      margin-right: 20px;
+      color: #999;
+      border-radius: 2px;
+      position: relative;
+      transition: all .3s;
+      &.active {
+        background: @xtxColor;
+        border-color: @xtxColor;
+        color: #fff;
+      }
+      .arrow {
+        position: absolute;
+        border: 5px solid transparent;
+        right: 8px;
+        &.up {
+          top: 3px;
+          border-bottom-color: #bbb;
+          &.active {
+            border-bottom-color: @xtxColor;
+          }
+        }
+        &.down {
+          top: 15px;
+          border-top-color: #bbb;
+          &.active {
+            border-top-color: @xtxColor;
+          }
+        }
+      }
+    }
+  }
+  .check {
+    .xtx-checkbox {
+      margin-left: 20px;
+      color: #999;
+    }
+  }
+}
+</style>
--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -5,6 +5,12 @@
       <sub-bread></sub-bread>
       <!-- 筛选区 -->
       <sub-filter></sub-filter>
+      <!-- 结果区 -->
+      <div class="goods-list">
+        <!-- 排序 -->
+        <sub-sort></sub-sort>
+        <!-- 列表 -->
+      </div>
     </div>
   </div>
 </template>
@@ -12,15 +18,21 @@
 <script>
 import SubBread from './components/sub-bread'
 import SubFilter from './components/sub-filter'
+import SubSort from './components/sub-sort'
 export default {
   name: 'SubPage',
   components: {
     SubBread,
-    SubFilter
+    SubFilter,
+    SubSort
   }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.goods-list {
+  background: #fff;
+  padding: 0 25px;
+  margin-top: 25px;
+}
 </style>
