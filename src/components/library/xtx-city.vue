569229a11cf9e697f71c14cfc61ecf2d281ba000
Lollipop <291950304@qq.com>
2021-08-12 22:50:13 +0800

    城市组件获取地区数据

--- src/components/library/xtx-city.vue
+++ src/components/library/xtx-city.vue
@@ -1,43 +1,81 @@
 <template>
 <div class="xtx-city" ref="target">
   <div class="select" :class="{ active }" @click="toggleDialog">
-    <span class="placeholder">请选择配送地址</span>
+    <span class="placeholder">{{ placeholder }}</span>
     <span class="value"></span>
     <i class="iconfont icon-angle-down"></i>
   </div>
   <div class="option" v-if="active">
-    <span v-for="i in 24" :key="i">北京市</span>
+    <div class="loading" v-if="loading"></div>
+    <template v-else>
+      <span v-for="item in currList" :key="item.code">{{ item.name }}</span>
+    </template>
   </div>
 </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import { onClickOutside } from '@vueuse/core'
+import axios from 'axios'
+
 export default {
   name: 'XtxCity',
+  props: {
+    placeholder: {
+      type: String,
+      default: '请选择配送地址'
+    }
+  },
   setup () {
     const target = ref(null)
+    // 显示隐藏
     const active = ref(false)
+    // 加载数据
+    const loading = ref(false)
+    // 显示的数据
+    const cityData = ref([])
+    // 显示隐藏函数
     const openDialog = () => {
       active.value = true
+      loading.value = true
+      getCityData().then(data => {
+        cityData.value = data
+        loading.value = false
+      })
     }
     const closeDialog = () => {
       active.value = false
     }
     const toggleDialog = () => {
-      if (active.value === true) {
-        closeDialog()
-      } else {
-        openDialog()
-      }
+      active.value ? closeDialog() : openDialog()
     }
     // 如果点击其他地方，关闭弹层
     onClickOutside(target, () => {
       closeDialog()
     })
-    return { target, toggleDialog, active }
+    // 显示点击后的数据
+    const currList = computed(() => {
+      return cityData.value
+    })
+    // console.log(currList)
+    return { target, toggleDialog, active, loading, currList }
   }
+}
+// 获取城市地区数据函数
+const getCityData = () => {
+  return new Promise((resolve, reject) => {
+    if (window.cityData) {
+      // 假如有缓存
+      resolve(window.cityData)
+    } else {
+      const url = 'https://yjy-oss-files.oss-cn-zhangjiakou.aliyuncs.com/tuxian/area.json'
+      axios.get(url).then(res => {
+        window.cityData = res.data
+        resolve(window.cityData)
+      })
+    }
+  })
 }
 </script>
 
@@ -88,6 +126,11 @@ export default {
       &:hover {
         background: #f5f5f5;
       }
+    }
+    .loading {
+      height: 290px;
+      width: 100%;
+      background: url(../../assets/images/loading.gif) no-repeat center;
     }
   }
 }
