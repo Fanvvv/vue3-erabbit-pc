cebb34734c41f1a93355be41a58e945bf36e2d62
Lollipop <291950304@qq.com>
2021-08-15 22:43:16 +0800

    增加获取商品同类推荐的方法

--- src/api/product.js
+++ src/api/product.js
@@ -11,3 +11,16 @@ export const findGoods = (id) => {
     { id }
   )
 }
+
+/**
+ * 获取商品同类推荐
+ * @param id 商品ID（传入代表查询相关商品，不传代表查询猜你喜欢）
+ * @param limit 数量限制，默认值为 4
+ */
+export const findGoodsRelevant = (id, limit) => {
+  return request(
+    '/goods/relevant',
+    'get',
+    { id, limit }
+  )
+}
