48236cd4a3967df4575c6c71510d42e4a1a09126
Lollipop <291950304@qq.com>
2021-08-15 23:00:34 +0800

    增加获取热榜商品方法

--- src/api/product.js
+++ src/api/product.js
@@ -24,3 +24,16 @@ export const findGoodsRelevant = (id, limit) => {
     { id, limit }
   )
 }
+
+/**
+ * 获取热榜商品
+ * @param {String} id - 商品ID
+ * @param {Number} type - 1代表24小时热销榜 2代表周热销榜 3代表总热销榜
+ * @param {Number} limit - 获取个数
+ */
+export const findHotGoods = (id, type, limit = 3) => {
+  return request(
+    '/goods/hot',
+    'get',
+    { id, type, limit })
+}
