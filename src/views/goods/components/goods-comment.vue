dc5275623d641c5d16586c96d7fe69abd7d6db71
Lollipop <291950304@qq.com>
2021-08-17 13:49:21 +0800

    完成评价组件的头部渲染

--- src/views/goods/components/goods-comment.vue
+++ src/views/goods/components/goods-comment.vue
@@ -1,15 +1,147 @@
 <template>
   <div class="goods-comment">
-    评论
+    <div class="head" v-if="commentInfo">
+      <div class="data">
+        <p><span>{{ commentInfo.salesCount }}</span><span>人购买</span></p>
+        <p><span>{{ commentInfo.praisePercent }}</span><span>好评率</span></p>
+      </div>
+      <div class="tags">
+        <div class="dt">大家都在说：</div>
+        <div class="dd">
+          <a
+            href="javascript:;"
+            v-for="(item, i) in commentInfo.tags"
+            :key="item.title"
+            :class="{ active: currentTagIndex === i }"
+            @click="changeTag(i)">{{ item.title }}（{{ item.tagCount }}）</a>
+        </div>
+      </div>
+    </div>
+    <div class="sort">
+      <span>排序：</span>
+      <a href="javascript:;" class="active">默认</a>
+      <a href="javascript:;">最新</a>
+      <a href="javascript:;">最热</a>
+    </div>
+    <div class="list"></div>
   </div>
 </template>
 
 <script>
+import { ref, inject } from 'vue'
+import { findGoodsComment } from '@/api/product'
+
+const getCommentInfo = (id) => {
+  const commentInfo = ref(null)
+  findGoodsComment(id).then(({ result }) => {
+    // 添加两条数据
+    result.tags.unshift({
+      type: 'img',
+      title: '图片',
+      tagCount: result.hasPictureCount
+    })
+    result.tags.unshift({
+      type: 'all',
+      title: '全部评价',
+      tagCount: result.evaluateCount
+    })
+    commentInfo.value = result
+  })
+  return commentInfo
+}
 export default {
-  name: 'GoodsComment'
+  name: 'GoodsComment',
+  setup () {
+    const goods = inject('goods')
+    const commentInfo = getCommentInfo(goods.id)
+    const currentTagIndex = ref(0)
+    const changeTag = (i) => {
+      currentTagIndex.value = i
+    }
+    return { commentInfo, currentTagIndex, changeTag }
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.goods-comment {
+  .head {
+    display: flex;
+    padding: 30px 0;
+    .data {
+      width: 340px;
+      display: flex;
+      padding: 20px;
+      p {
+        flex: 1;
+        text-align: center;
+        span {
+          display: block;
+          &:first-child {
+            font-size: 32px;
+            color: @priceColor;
+          }
+          &:last-child {
+            color: #999;
+          }
+        }
+      }
+    }
+    .tags {
+      flex: 1;
+      display: flex;
+      border-left: 1px solid #f5f5f5;
+      .dt {
+        font-weight: bold;
+        width: 100px;
+        text-align: right;
+        line-height: 42px;
+      }
+      .dd {
+        flex: 1;
+        display: flex;
+        flex-wrap: wrap;
+        > a {
+          width: 132px;
+          height: 42px;
+          margin-left: 20px;
+          margin-bottom: 20px;
+          border-radius: 4px;
+          border: 1px solid #e4e4e4;
+          background: #f5f5f5;
+          color: #999;
+          text-align: center;
+          line-height: 40px;
+          &:hover {
+            border-color: @xtxColor;
+            background: lighten(@xtxColor,50%);
+            color: @xtxColor;
+          }
+          &.active {
+            border-color: @xtxColor;
+            background: @xtxColor;
+            color: #fff;
+          }
+        }
+      }
+    }
+  }
+  .sort {
+    height: 60px;
+    line-height: 60px;
+    border-top: 1px solid #f5f5f5;
+    border-bottom: 1px solid #f5f5f5;
+    margin: 0 20px;
+    color: #666;
+    > span {
+      margin-left: 20px;
+    }
+    > a {
+      margin-left: 30px;
+      &.active,&:hover {
+        color: @xtxColor;
+      }
+    }
+  }
+}
 </style>
