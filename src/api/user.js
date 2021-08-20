81fe8bb96b3b5b4c15c8d0749efa0f3d2c5a044f
Lollipop <291950304@qq.com>
2021-08-20 23:18:09 +0800

    添加短信登录和获取验证码方法

--- src/api/user.js
+++ src/api/user.js
@@ -13,3 +13,28 @@ export const userAccountLogin = ({ account, password }) => {
     { account, password }
   )
 }
+
+/**
+ * 短信登录
+ * @param {String} mobile - 手机号
+ * @param {String} code - 验证码
+ * @returns Promise
+ */
+export const userMobileLogin = ({ mobile, code }) => {
+  return request(
+    '/login/code',
+    'post',
+    { mobile, code })
+}
+
+/**
+ * 获取短信登录验证码
+ * @param {String} mobile - 手机号
+ * @returns Promise
+ */
+export const userMobileLoginMsg = (mobile) => {
+  return request(
+    '/login/code',
+    'get',
+    { mobile })
+}
