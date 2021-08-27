63613ef1f1a7ff20c38a8eaea104fd54479c7b48
Lollipop <291950304@qq.com>
2021-08-27 23:31:59 +0800

    添加提交订单的方法

--- src/api/order.js
+++ src/api/order.js
@@ -33,3 +33,15 @@ export const editAddress = (address) => {
     address
   )
 }
+
+/**
+ * 提交订单
+ * @param {Object} order - 订单信息对象
+ */
+export const createOrder = (order) => {
+  return request(
+    '/member/order',
+    'post',
+    order
+  )
+}
