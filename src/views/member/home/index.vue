8bb610545f61d1193892d0c6673fb36f0ec3a423
Lollipop <291950304@qq.com>
2021-08-29 22:17:53 +0800

    完成个人中心首页的基本布局

--- src/views/member/home/index.vue
+++ src/views/member/home/index.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="member-home">
-    个人中心
+    <!-- 概览 -->
+    <home-overview></home-overview>
+    <!-- 收藏 -->
+    <home-panel title="我的收藏"></home-panel>
+    <!-- 足迹 -->
+    <home-panel title="我的足迹"></home-panel>
+    <!-- 猜你 -->
+    <goods-relevant></goods-relevant>
   </div>
 </template>
 
 <script>
+import MemberHomeOverview from './components/home-overview'
+import MemberHomePanel from './components/home-panel'
+import GoodsRelevant from '@/views/goods/components/goods-relevant'
+
 export default {
-  name: 'MemberHome'
+  name: 'MemberHome',
+  components: {
+    HomeOverview: MemberHomeOverview,
+    HomePanel: MemberHomePanel,
+    GoodsRelevant
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+:deep(.xtx-carousel) .carousel-btn.prev {
+  left: 5px;
+}
+:deep(.xtx-carousel) .carousel-btn.next {
+  right: 5px;
+}
 </style>
