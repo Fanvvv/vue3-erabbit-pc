918bf3e74fea8e3777af2b090a81e3e732623aa4
Lollipop <291950304@qq.com>
2021-08-31 21:27:38 +0800

    添加取消订单的方法

--- src/api/order.js
+++ src/api/order.js
@@ -71,3 +71,17 @@ export const findOrderList = ({ orderState, page, pageSize }) => {
     { orderState, page, pageSize }
   )
 }
+
+/**
+ * 取消订单
+ * @param {String} orderId - 订单ID
+ * @param {String} cancelReason - 取消原因
+ * @returns Promise
+ */
+export const cancelOrder = (orderId, cancelReason) => {
+  return request(
+    `/member/order/${orderId}/cancel`,
+    'put',
+    { cancelReason }
+  )
+}
