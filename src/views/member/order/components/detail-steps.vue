ebd3a38a8a1339f6c1b1a9b7e7fba8d806e54568
Lollipop <291950304@qq.com>
2021-09-01 22:02:08 +0800

    封装一个高可用的步骤条组件

---
+++ src/views/member/order/components/detail-steps.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="detail-steps" style="padding:20px">
+    <xtx-steps :active="order.orderState === 6 ? 1 : order.orderState">
+      <xtx-steps-item title="提交订单" :desc="order.createTime"></xtx-steps-item>
+      <xtx-steps-item title="付款成功" :desc="order.payTime"></xtx-steps-item>
+      <xtx-steps-item title="商品发货" :desc="order.consignTime"></xtx-steps-item>
+      <xtx-steps-item title="确认收货" :desc="order.evaluationTime"></xtx-steps-item>
+      <xtx-steps-item title="订单完成" :desc="order.endTime"></xtx-steps-item>
+    </xtx-steps>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailSteps',
+  props: {
+    order: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.detail-steps {
+  padding: 20px;
+}
+</style>
--- src/views/member/order/components/order-detail.vue
+++ src/views/member/order/components/order-detail.vue
@@ -3,6 +3,7 @@
     <!-- 操作栏 -->
     <detail-action :order="order"></detail-action>
     <!-- 步骤条 组件xtx-steps.vue-->
+    <detail-steps :order="order"></detail-steps>
     <!-- 物流栏 -->
     <!-- 订单商品信息 -->
   </div>
@@ -13,11 +14,13 @@ import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { findOrder } from '@/api/order'
 import DetailAction from './detail-action'
+import DetailSteps from './detail-steps'
 
 export default {
   name: 'MemberOrderDetail',
   components: {
-    DetailAction
+    DetailAction,
+    DetailSteps
   },
   setup () {
     const order = ref(null)
